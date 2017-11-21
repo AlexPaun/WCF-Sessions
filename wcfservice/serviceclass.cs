30209178aed9edd84cb36266d77d174f84a63bb7
AlexPaun <pmalex_96@yahoo.com>
2017-11-21 11:43:35 +0100

    Add project files.

---
+++ wcfservice/serviceclass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.IO;
+using System.Data.SqlClient;
+using System.Transactions;
+
+namespace wcfservice
+{
+    [ServiceContract(SessionMode = SessionMode.Allowed)]
+    public interface Iservice
+    {
+        [OperationContract]     
+        void dowork();
+    }
+
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class serviceclass : Iservice
+    {
+        public void dowork()
+        {
+            Console.WriteLine("Instance Object : {0}", OperationContext.Current.InstanceContext.GetHashCode());
+            Console.WriteLine("Service Object : {0}", OperationContext.Current.InstanceContext.GetServiceInstance().GetHashCode());
+            Console.WriteLine("Session Id  : {0}", OperationContext.Current.SessionId);
+            Console.WriteLine();
+        }
+    }
+}
